33d270f631f5094c0aeb982f657590177b6e8872
baechanyong1 <110149885+baechanyong1@users.noreply.github.com>
2023-06-23 17:40:14 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// app.js
-
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", []);
+app.use("/api", [usersRouter]);
+app.use("/api", [postsRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, "포트 번호로 서버가 실행되었습니다.");
